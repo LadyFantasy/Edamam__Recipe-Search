7986bd1b407d68b0a6182bcb5a41d7af4f12cb4e
LadyFantasy <msoledadm88@gmail.com>
2020-07-01 12:01:34 +0100

    Final

--- Final_Project.py
+++ Final_Project.py
@@ -19,91 +19,56 @@
 #=================================================================================================================================
 
 
-# PSEUDOCODE
-# 1. Input(select a language EN for English / ES for Spanish)  DONE
-# 2. Select search: by ingredient / weekly meal planner   HALF DONE
-# Search by ingredient: show one random recipe for that ingredient   DONE
-# Weekly meal planner: input(What's your ideal daily intake of calories?) int*7:
-# show 14 random recipes that add up to the total
-
-
-
 import requests
 import random
 
-
-
-
-# function that searches all the recipes
-def recipe_search(ingredient, api):  
+# function that gets all the inputs from the user (language, type of search, ingredient and random/all recipes) and the first one to execute
+def run():   
+    # separates english and spanish navigation
+    language = input("Do you want to search in English or Spanish? ").lower()
     
-    results = get_api_data(api, ingredient)
-    
-    for result in results:
-        recipe = result['recipe']
-        # print("Recipe:")
-        print(recipe['label'])
-        # print("Link:")
-        print(recipe['uri'])
-        print("==========================")
- 
-
-# function that searches a random recipe
-def random_recipe_search(ingredient, api):
-    
-    all_recipes = get_api_data(api, ingredient)
-
-    random_number = random.randint(0, len(all_recipes)-1)    
-    random_recipe = all_recipes[random_number]
-    recipe = random_recipe["recipe"]
-
-      
-    print(recipe["label"])
-    print(recipe["uri"])
-    print("Ingredients:")
-    for ingredient in recipe["ingredientLines"]:
-        print(f"-{ingredient}")
-   
-   
- # function for meal planner
-def meal_planner(calories, api):
-    results_weekly = get_api_calorie_data(api, calories)
-    
-    # print calories for user
-    
-    for result in results_weekly:
-        recipe = result['recipe']
-        # print("Recipe:")
-        print(recipe['label'])
-        # print("Link:")
-        print(recipe['uri'])
-        print("==========================")
-
+    # SPANISH
+    if (language == "spanish" or language == "s"):
+        api = "test-es"
+        input_choice = input('Quiere elegir un ingrediente para buscar una receta (ingrese: I) o quiere elegir por cantidad de calorías(ingrese: C)? (I/C) ').lower()
         
-    
+        #recipe search
+        if (input_choice == 'i'):
+            ingredient = input('Elija un ingrediente: ')
+            input_random = input("¿Quiere una receta al azar (ingrese: R) o quiere revisar el catálogo (ingrese: C)? R/C  ").lower()
 
-# gets data from the API for the weekly meal planner
-def get_api_calorie_data(api, calories):
-    app_id = '71ca93f8'
-    app_key = '11b48d20765f456984566e814c328384'
-    calories_fetch = calories // 3
-    result = requests.get('https://{}.edamam.com/search?q=calories={}&app_id={}&app_key={}'.format(api, calories_fetch, app_id, app_key))
-    
-    data = result.json()["hits"]
-   
-    return data
-   
-   
-# gets data from the API for the recipe search
-def get_api_data(api, ingredient):
-    app_id = '71ca93f8'
-    app_key = '11b48d20765f456984566e814c328384'
-          
-    result = requests.get('https://{}.edamam.com/search?q={}&app_id={}&app_key={}'.format(api, ingredient, app_id,
-    app_key))
-    data = result.json()["hits"]
-    
-    return data
+        # calls the function that decides if the user wants all the recipes or a random recipe
+            random_ask(ingredient, api, input_random)
+            
+        #meal planner
+        elif (input_choice== 'c'):
+            calories = int(input("Cuántas calorías quiere consumir? "))
+            meal_planner(calories, api)
+        
+    # ENGLISH   
+    elif(language == "english" or language == "e"):
+        api = "api"  
+        input_choice = input('Do you want to choose by ingredient or by calories? i/c ').lower()
+        
+        # recipe search
+        if (input_choice == 'i'):
+            ingredient = input('Enter an ingredient: ')
+            input_random = input("Do you want a random recipe (enter: R) or do you want to check our catalogue (enter: C)? R/C ").lower()
+            
+            # calls the function that decides if the user wants all the recipes or a random recipe
+            random_ask(ingredient, api, input_random)
+        
+        # meal planner  
+        elif (input_choice == 'c'):
+            calories = int(input("what's the maximum calories for each meal? "))
+            meal_planner(calories, api)
+            
+                  
+    else:
+        print("Enter a valid language ")
+        # language = input("Do you want to search in English or Spanish? ")
+        run()
+
 
 
 #  function that divides the search between random recipe and all the recipes 
@@ -118,50 +83,140 @@ def random_ask(ingredient, api, input_random):
     else:
         input_random
     
+
+          
+
+# gets data from the API for the weekly meal planner in ENGLISH
+def get_api_calorie_data(api, calories):
+    app_id = '71ca93f8'
+    app_key = '11b48d20765f456984566e814c328384'
     
-# function that gets all the inputs from the user (language, type of search, ingredient and random/all recipes) and the first one to execute
-def run():   
-    # separates english and spanish navigation
-    language = input("Do you want to search in English or Spanish? ").lower()
+    result = requests.get('https://{}.edamam.com/search?q=calories={}&app_id={}&app_key={}'.format(api, calories, app_id, app_key))
     
-    # SPANISH
-    if (language == "spanish" or language == "s"):
-        api = "test-es"
-        ingredient = input('Elija un ingrediente: ')
-        input_random = input("¿Quiere una receta al azar (ingrese: R) o quiere revisar el catálogo (ingrese: C)? R/C ").lower()
+    data = result.json()["hits"]
+   
+    return data
+
+
+# gets data from the API for the weekly meal planner in SPANISH
+def get_api_calorie_data_sp(api, calories):
+    app_id = '71ca93f8'
+    app_key = '11b48d20765f456984566e814c328384'
+    result = requests.get('https://{}.edamam.com/search?q=calorias={}&app_id={}&app_key={}'.format(api, calories, app_id, app_key))
+    
+    data = result.json()["hits"]
+   
+    return data
+  
+   
+# gets data from the API for the recipe search
+def get_api_data(api, ingredient):
+    app_id = '71ca93f8'
+    app_key = '11b48d20765f456984566e814c328384'
+    result = requests.get('https://{}.edamam.com/search?q={}&app_id={}&app_key={}'.format(api, ingredient, app_id,
+    app_key))
+    data = result.json()["hits"]
+    
+    return data
+
+   
+    
+
+# function that prints all the recipes
+def recipe_search(ingredient, api):  
+    
+    results = get_api_data(api, ingredient)
+ 
+ #prints in console   
+    for result in results:
+        recipe = result['recipe']
+        # print("Recipe:")
+        print(recipe['label'])
+        # print("Link:")
+        print(recipe['uri'])
+        print("==========================")
         
-        # calls the function that decides if the user wants all the recipes or a random recipe
-        random_ask(ingredient, api, input_random)
         
-    # ENGLISH   
-    elif(language == "english" or language == "e"):
-        api = "api"  
-        input_choice = input('Do you want to choose by recipe or by weekly planner? r/w').lower()
-        
-        # recipe search
-        if (input_choice == 'r'):
-            ingredient = input('Enter an ingredient: ')
-            input_random = input("Do you want a random recipe (enter: R) or do you want to check our catalogue (enter: C)? R/C ").lower()
+#saves recipes in a file
+    with open("Recipes.txt", "a") as Recipes:
+        for result in results:
+            recipe = result['recipe']
+            Recipes.write("\n")
+            Recipes.write(f"{recipe['label']}\n")
+            Recipes.write(f"URL: {recipe['uri']}\n")
+            Recipes.write("\n")
+            Recipes.write("========================================")
             
-            # calls the function that decides if the user wants all the recipes or a random recipe
-            random_ask(ingredient, api, input_random)
-        
-        # meal planner  
-        elif (input_choice == 'w'):
-            calories = int(input("what's your maximum daily calories?"))
-            meal_planner(calories, api)
-            
-                  
-    else:
-        print("Enter a valid language")
-        # language = input("Do you want to search in English or Spanish? ")
-        run()
-        
+        Recipes.close()        
     
+ 
+ 
+
+# function that prints a random recipe
+def random_recipe_search(ingredient, api):
+    
+    all_recipes = get_api_data(api, ingredient)
+
+    random_number = random.randint(0, len(all_recipes)-1)    
+    random_recipe = all_recipes[random_number]
+    recipe = random_recipe["recipe"]
+
+#prints in console 
+    print("\n")
+    print(recipe["label"])
+    print("Ingredients:")
+    for ingredient in recipe["ingredientLines"]:
+        print(f"-{ingredient}")
+        
+#saves recipe in a file
+    with open("Recipes_random.txt", "a") as Recipes_random:
+            Recipes_random.write("\n")
+            Recipes_random.write(f"{recipe['label']}\n\n")
+            for ingredient in recipe["ingredientLines"]:
+                Recipes_random.write(f"-{ingredient}\n")
+            Recipes_random.write("\n")
+            Recipes_random.write("========================================")
+            
+            Recipes_random.close()
+   
+
+ 
+ # function for meal planner
+def meal_planner(calories, api):
+    if api == "test-es":
+        results_weekly = get_api_calorie_data_sp(api, calories)
+    else: 
+        results_weekly = get_api_calorie_data(api, calories)
+
+   
+    # prints in console
+    for result in results_weekly[0:3]:
+        recipe = result['recipe']
+        # print("Recipe:")
+        print(recipe['label'])
+        print("------------------------------")
+        print("Ingredients:" ) 
+        for ingredient in recipe["ingredientLines"]:
+            print(f"-{ingredient}")
+        print("=============================")
+        print("\n")
+    
+    #saves the recipes in a file      
+        with open("Meal_planner.txt", "a") as Meal_planner:
+            Meal_planner.write("\n")
+            Meal_planner.write(f"{recipe['label']}\n\n")
+            for ingredient in recipe["ingredientLines"]:
+                Meal_planner.write(f"-{ingredient}\n")
+            Meal_planner.write("\n")
+            Meal_planner.write("========================================")
+            
+            Meal_planner.close()
+           
+
+    
+ 
     
 # *******program starts here********          
 run()
-
-
 
 
